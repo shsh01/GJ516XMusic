53c7fcf373777389bb2ca6c234cd4070701fe38e
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-22 15:40:07 +0530

    Update settings.py

--- GJ516X/utils/inline/settings.py
+++ GJ516X/utils/inline/settings.py
@@ -8,23 +8,23 @@ def setting_markup(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ", callback_data="AQ"
+                text="✧ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ✧", callback_data="AQ"
             ),
             InlineKeyboardButton(
-                text="ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ", callback_data="VQ"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴀᴜᴛʜ ᴜsᴇʀs", callback_data="AU"
-            ),
-            InlineKeyboardButton(
-                text="ᴄʟᴇᴀɴ ᴍᴏᴅᴇ", callback_data="CM"
+                text="✧ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ✧", callback_data="VQ"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ •", url=f"{SUPPORT_GROUP}",
+                text="✧ᴀᴜᴛʜ ᴜsᴇʀs✧", callback_data="AU"
+            ),
+            InlineKeyboardButton(
+                text="✧ᴄʟᴇᴀɴ ᴍᴏᴅᴇ✧", callback_data="CM"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✧sᴜᴩᴩᴏʀᴛ✧", url=f"{SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
