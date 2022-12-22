7300627b76e6055b244f7b9a635169e4dc372ba4
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2022-12-22 16:40:06 +0530

    Update start.py

--- GJ516X/plugins/Robot/start.py
+++ GJ516X/plugins/Robot/start.py
@@ -39,7 +39,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CZp7eIAACaD5jT_B5ItSk5M33SkPc2AProK4GIgACdAYAArJscVY0KNBsR3kYDh4E")
+            await message.reply_sticker("CAACAgUAAx0CZp7eIAACqcljpDql4xuawBT-rKCuETMe31pvZwAChwYAAgj0QFZqmXkJ3SZqwh4E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -188,7 +188,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAx0CZp7eIAACaD5jT_B5ItSk5M33SkPc2AProK4GIgACdAYAArJscVY0KNBsR3kYDh4E")
+                await message.reply_sticker("CAACAgUAAx0CZp7eIAACqcljpDql4xuawBT-rKCuETMe31pvZwAChwYAAgj0QFZqmXkJ3SZqwh4E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
